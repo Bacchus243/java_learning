6f242809f39cc08bcb939247ca5c578f8f6d831f
Trenton Meryhew <tmeryhewjsf35@gmail.com>
2022-08-09 12:52:04 -0700

    Fully built out Pizza Party planner app

--- module03/src/Assignment01_Pizza_Party.java
+++ module03/src/Assignment01_Pizza_Party.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class Assignment01_Pizza_Party {
@@ -6,22 +5,50 @@ public class Assignment01_Pizza_Party {
         // declare and instantiate the Scanner
         Scanner scan = new Scanner(System.in);
         
-        
         // declare and initialize the variables
-       
         
+        		// inputs
+        int slicesPerPizza = 0;
+        int numPizzas = 0;
+        int numAdults = 0;
+        int numChildren = 0;
         
+                
+        		// outputs
+        int totalSlices = 0;
+        int slicesForAdults = 0;
+        int slicesForChildren = 0;
+        int slicesPerChild = 0;
+        int slicesRemaining = 0;
+        
+                
         // prompt for and collect the inputs
-        
-        
+        System.out.print("Number of slices per pizza : ");
+        slicesPerPizza = scan.nextInt();
+        System.out.print("Number of pizzas purchased : ");
+        numPizzas = scan.nextInt();
+        System.out.print("Number of adults           : ");
+        numAdults = scan.nextInt();
+        System.out.print("Number of children         : ");
+        numChildren = scan.nextInt();
+        System.out.println("\n\n");
+
         
         // compute the required values
-      
+        totalSlices = slicesPerPizza * numPizzas;
+        slicesForAdults = 3 * numAdults;        
+        slicesForChildren = totalSlices - slicesForAdults;        
+        slicesPerChild = slicesForChildren / numChildren;        
+        slicesRemaining = slicesForChildren - (slicesPerChild * numChildren);
         
         
+
         // display the required outputs
-       
-        
+        System.out.printf("Total number of slices of pizza               : %d\n", totalSlices);
+        System.out.printf("total number of slices given to adults        : %d\n", slicesForAdults);
+        System.out.printf("Total number of slices available for children : %d\n", slicesForChildren);
+        System.out.printf("Number of slices each child will get          : %d\n", slicesPerChild);
+        System.out.printf("Number of slices left over                    : %d\n", slicesRemaining);        
         
         scan.close();
     }
