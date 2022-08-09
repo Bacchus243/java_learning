482b393f912bbde4f6adf55106c2d750c1424564
Trenton Meryhew <tmeryhewjsf35@gmail.com>
2022-08-09 11:16:31 -0700

    created Assignment01_Pizza_Party program

---
+++ module03/src/Assignment01_Pizza_Party.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Assignment01_Pizza_Party {
+    public static void main(String[] args) {  
+        // declare and instantiate the Scanner
+        Scanner scan = new Scanner(System.in);
+        
+        
+        // declare and initialize the variables
+       
+        
+        
+        // prompt for and collect the inputs
+        
+        
+        
+        // compute the required values
+      
+        
+        
+        // display the required outputs
+       
+        
+        
+        scan.close();
+    }
+}
