44425b82d9bb9451fc5df678562ef21ab72a32eb
Trenton Meryhew <tmeryhewjsf35@gmail.com>
2022-08-09 08:43:18 -0700

    practiced with if statements and using boolean arguments

---
+++ module03/src/Testing_boolean_arguments.java
@@ -0,0 +1,19 @@
+
+public class Testing_boolean_arguments {
+	
+	public static void main(String[] args) {
+		boolean status = true;
+		
+		
+		if (!status) {
+			
+			System.out.println("Hi");	//if Hi is printed, the if statement proceeded with a false boolean argument
+		}
+		else {
+			System.out.println("Lisa");		//the if statement would not work with the false boolean argument
+		}
+		
+	}
+	
+
+}
