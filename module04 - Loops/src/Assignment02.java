8366735a507277fee60d77f48d093144794eed72
Trenton Meryhew <tmeryhewjsf35@gmail.com>
2022-08-10 16:02:17 -0700

    Created Lab3 and Assignment02 files

---
+++ module04 - Loops/src/Assignment02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Assignment02 {
+    public static void main(String[] args) {  
+        // declare and instantiate the Scanner
+        
+        
+    	// declare and initialize the variables
+        
+        
+        // prompt for and collect the inputs
+        
+        
+        // compute the required values
+        
+    	
+        // display the required outputs
+        
+    	
+    }
+}
---
+++ module04 - Loops/src/Credit_Card_Processor.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Credit_Card_Processor {
+
+	public static void main(String[] args) {
+		// Inputs
+				// this section is going to initialize the variables and prompt the user for starting values is required
+		Scanner in = new Scanner(System.in);
+		String = 
+		
+		System.out.print("Enter your credit card number: ");
+		
+		
+		
+		
+		
+		
+		// Processing
+				// this is where all of the calculations are going to be performed on the inputs
+		
+				// some type of Loop here
+		
+		
+		// Outputs
+				// finally, the results of the processing section are going to be displayed by this section if required
+		
+				// print statement to show the user their credit card with only numbers used
+		
+		in.close();
+	}
+
+}
